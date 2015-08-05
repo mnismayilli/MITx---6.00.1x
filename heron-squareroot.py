195d9d0e167b172dfe4e2c5b7ab10159d2af538b
Candyer <candyer@example.com>
2015-08-05 22:17:18 +0200

    the ancient square root

--- heron-squareroot.py
+++ heron-squareroot.py
@@ -1,4 +1,10 @@
 def square_root(x):
+	"""
+	this is the ancient way to calculate the square_root
+	step 1: you guess a number g, and get the result of g**2
+	step 2: if it's not close enough to X, take a new guess "g" with the formula below
+	step 3: go back to step 2 until it's close enough.
+	"""
 	g = 42.0
 	while g * g - x < -0.001 or g * g - x > 0.001:
 		g = (g + x / g) / 2
