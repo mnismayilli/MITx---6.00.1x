c371430cc9308e624c3da6ee5af42f9abdba835f
Candyer <candyer@example.com>
2015-09-12 22:16:56 +0200

    bisection search and recursion together

---
+++ isIn.py
@@ -0,0 +1,24 @@
+def isIn(char, aStr):
+	'''
+	char: a single character
+	aStr: an alphabetized string
+	returns: True if char is in aStr; False otherwise
+	'''
+	if aStr == "":
+		return False
+	elif len(aStr) == 1:
+		if char == aStr:
+			return True
+		return False
+	else:
+		mid = len(aStr)/2
+		if char == aStr[mid]:
+			return True
+		elif char > aStr[mid]:
+			return isIn(char, aStr[mid:])
+		else: 
+			return isIn(char, aStr[:mid])
+
+print isIn("m", "acdglmnqyz")
+print isIn('o', '')
+print isIn('a','c')
