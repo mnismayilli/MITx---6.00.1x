1063f7b5aca124d5f5cdc5a269ae40447d94c5b3
Candyer <candyer@example.com>
2015-09-28 17:23:57 +0200

    extra practice with recursion

---
+++ pp2.py
@@ -0,0 +1,22 @@
+#1
+def sharp(x):
+	if x == 2:
+		return 2
+	return  (sharp(x-1))**x
+
+print sharp(2)  # 2
+print sharp(4)  # 4096
+print sharp(10)  # it's gonna a suuuuuuuuper big number!
+
+#2
+def ndigits(x):
+	if x/10 == 0:
+		return 1
+	return 1 + ndigits(x/10)
+
+print ndigits(6)  # 1
+print ndigits(3456789)  # 7
+print ndigits(1234567890098765432345678986546)  # 31
+
+#3
+print ndigits(sharp(7)) + 2*ndigits(sharp(6)) + ndigits(sharp(5)) + ndigits(sharp(4))  #1000
