a81608d56819f5d84e5035a30ad75980f80e0c70
candyer <candyer@users.noreply.github.com>
2016-08-08 12:11:24 +0200

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -6,18 +6,44 @@ class node():
     def __str__(self):
     	return self.value
 
-e = node("e", None, None)
-g = node("g", None, None)
-m = node("m", None, None)
-t = node("t", None, None)
-a = node("a", e, g)
-e = node("e", m, None)
-h = node("h", t, None)
-O = node("!", a, e)  
-P = node(":)", h, O) 
-root = P
-# ! and :) cannot be the name,so i name them O and P
+# e = node("e", None, None)
+# g = node("g", None, None)
+# m = node("m", None, None)
+# t = node("t", None, None)
+# a = node("a", e, g)
+# e = node("e", m, None)
+# h = node("h", t, None)
+# O = node("!", a, e)  
+# P = node(":)", h, O) 
+# root = P
+# # ! and :) cannot be the name,so i name them O and P
 
+g = node("G", None, None)
+f = node("F", g, None)
+e = node("E", None, None)
+d = node("D", None, e)
+c = node("C", None, None)
+b = node("B", c, d)  
+a = node("A", b, f) 
+root = a
+
+# pre_order
+def DFS(node):
+	print node,
+	if node.left:
+		DFS(node.left)
+	if node.right:
+		DFS(node.right)
+
+# in_order
+def DFS(node):
+	if node.left:
+		DFS(node.left)
+	print node,
+	if node.right:
+		DFS(node.right)
+		
+# post_order
 def DFS(node):
 	if node.left:
 		DFS(node.left)
